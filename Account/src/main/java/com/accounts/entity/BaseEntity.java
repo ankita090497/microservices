e269934ba02ad1d83f103b6b030b7cb090650843
ankita090497 <ankita090497@gmail.com>
2025-04-06 17:22:14 +0530

    Account service : Create API has been updated

--- Account/src/main/java/com/accounts/entity/BaseEntity.java
+++ Account/src/main/java/com/accounts/entity/BaseEntity.java
@@ -18,19 +18,15 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
 
-    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
-    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
-    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
--- Account/src/main/java/com/accounts/service/impl/AccountServiceImpl.java
+++ Account/src/main/java/com/accounts/service/impl/AccountServiceImpl.java
@@ -1,12 +1,20 @@
 package com.accounts.service.impl;
 
+import com.accounts.constant.AccountsConstants;
 import com.accounts.dto.CustomerDto;
+import com.accounts.entity.Accounts;
+import com.accounts.entity.Customer;
+import com.accounts.exception.CustomerAlreadyExistsException;
+import com.accounts.mapper.CustomerMapper;
 import com.accounts.repository.AccountsRepository;
 import com.accounts.repository.CustomerRepository;
 import com.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -21,6 +29,25 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber " + customerDto.getMobileNumber());
+        }
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
 
+
+    }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts accounts = new Accounts();
+        accounts.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        accounts.setAccountNumber(randomAccNumber);
+        accounts.setAccountType(AccountsConstants.SAVINGS);
+        accounts.setBranchAddress(AccountsConstants.ADDRESS);
+        return accounts;
     }
 }
